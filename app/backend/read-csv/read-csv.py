b2d3c28481529be0edbdc3312ee57c2e5b1a6a68
J2yun <alex68566856@gmail.com>
2023-07-21 13:43:53 +0900

    modify read-csv

--- app/backend/read-csv/read-csv.py
+++ app/backend/read-csv/read-csv.py
@@ -1,7 +1,7 @@
 import json
+import boto3
 import io
 import pandas as pd
-import boto3
 
 def lambda_handler(event, context):
     BUCKET_NAME="jiyun-seoul-serverless-keb"
@@ -14,11 +14,18 @@ def lambda_handler(event, context):
         )
     df = pd.read_csv(io.BytesIO(obj["Body"].read()), index_col=0)
     
-    
-    
-    
-    # TODO implement
+    data = {
+        "Index" : list(df['Index']),
+        "Date" : list(df['Date']),
+        "Type" : list(df['Type']),
+        "Category" : list(df['Category']),
+        "Item" : list(df['Item']),
+        "Amount" : list(df['Amount']),
+        "Note" : list(df['Note'])   
+        }
+
+    #TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('add Data Successfully!')
+        'body': json.dumps(data),
     }
